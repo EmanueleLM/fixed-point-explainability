c63df94965be132a9ad4adb3c8c355387cfbde76
Emanuele La Malfa <emalfa@CSL15744.local>
2025-05-16 15:27:23 +0100

    first commit

---
+++ prototype-based/setup.sh
@@ -0,0 +1,9 @@
+mkdir -p results/protovae
+mkdir -p results/prototypeDNN
+
+mkdir -p data/fashion
+cd data/fashion
+wget http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz
+wget http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz
+wget http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz
+wget http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz
